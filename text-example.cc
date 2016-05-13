a76a1e6d9867bd6850ad8f13aa9338f343bc7b8a
paul rosset <paul@rsst.eu>
2016-05-13 16:10:19 +0000

    zweizeiliges display mit Farbwechsel tut

--- text-example.cc
+++ text-example.cc
@@ -12,9 +12,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/poll.h>
+#include <sys/ioctl.h>
+#include <stropts.h>
 using namespace rgb_matrix;
 
+Color convertangletocolor(int angle);
 static int usage(const char *progname) {
   fprintf(stderr, "usage: %s [options]\n", progname);
   fprintf(stderr, "Reads text from stdin and displays it. "
@@ -121,10 +124,10 @@ int main(int argc, char *argv[]) {
   all_extreme_colors &= color.r == 0 || color.r == 255;
   all_extreme_colors &= color.g == 0 || color.g == 255;
   all_extreme_colors &= color.b == 0 || color.b == 255;
-  if (all_extreme_colors)
-    canvas->SetPWMBits(1);
+  //if (all_extreme_colors)
+  //  canvas->SetPWMBits(1);
 
-  const int x = x_orig;
+  int x = x_orig;
   int y = y_orig;
 
   if (isatty(STDIN_FILENO)) {
@@ -132,25 +135,103 @@ int main(int argc, char *argv[]) {
     printf("Enter lines. Full screen or empty line clears screen.\n"
            "Supports UTF-8. CTRL-D for exit.\n");
   }
+  char ndline[1024] = "CC2016 Darmstadt";
+  char line[1024] = "Energies Technologies";
 
-  char line[1024];
-  while (fgets(line, sizeof(line), stdin)) {
+    int angle = 0;
+  //while (fgets(line, sizeof(line), stdin)) {
+  while (1) {
+    
+    y = 2;
+    struct pollfd fds; 
+    int ret;
+    fds.fd = 0; /* this is STDIN */
+    fds.events = POLLIN;
+    ret = poll(&fds, 1, 0);
+    int max = 0;
+    if(ret == 1)
+    {
+      // we have exactly n bytes to read 
+      strcpy(ndline,line);   
+      fgets(line, sizeof(line), stdin);
     const size_t last = strlen(line);
-    if (last > 0) line[last - 1] = '\0';  // remove newline.
-    bool line_empty = strlen(line) == 0;
-    if ((y + font.height() > canvas->height()) || line_empty) {
-      canvas->Clear();
-      y = y_orig;
-    }
-    if (line_empty)
-      continue;
-    rgb_matrix::DrawText(canvas, font, x, y + font.baseline(), color, line);
-    y += font.height();
-  }
 
+    if (last > 0) line[last - 1] = '\0';  // remove newline.
+    }
+    canvas->Clear();
+    angle++;
+    Color c = convertangletocolor(angle); 
+    Color ndc = convertangletocolor(angle+180); 
+    int last = rgb_matrix::DrawText(canvas, font, x, y + font.baseline(), c, line);
+    y += 2+ font.height();
+    int ndlast = rgb_matrix::DrawText(canvas, font, x, y +  font.baseline(), ndc, ndline);
+     if ( last > ndlast)
+    {
+       max=last;
+    }
+    else
+    {
+       max=ndlast;
+    }
+    x-=1;
+    if(x < - (max))
+    {
+       x=128;
+    }
+    else{ max = ndlast;}
+    usleep(20000);
+  }
   // Finished. Shut down the RGB matrix.
   canvas->Clear();
   delete canvas;
 
   return 0;
+}
+Color convertangletocolor(int angle)
+    {
+                angle%=360;
+		unsigned char seg = angle / 60;
+		unsigned int f = (angle % 60) * 255 /59 ;
+		unsigned char t = 255 - f;
+       //         printf("seg = %d f= %d t= %d\n" , seg, f, t); 
+                int r, g, b;
+		switch (seg)
+		{
+		case 0:
+		default:
+			r = 255;
+			g = f;
+			b = 0;
+			break;
+
+		case 1:
+			r = t;
+			g = 255;
+			b = 0;
+			break;
+		case 2:
+			r = 0;
+			g = 255;
+			b = f;
+			break;
+		case 3:
+			r = 0;
+			g = t;
+			b = 255;
+			break;
+		case 4:
+			r = f;
+			g = 0;
+			b = 255;
+			break;
+		case 5:
+
+			r = 255;
+			g = 0;
+			b = t;
+			break;
+		}
+                Color ret(r,g,b);
+                return ret;
+     	
 }
